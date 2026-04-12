f80cda5cf95e86fe9f14d50c7f28be233ae1005e
Dhrimanti <dhritimancg@gmail.com>
2026-04-12 14:56:50 +0530

    Done with the kernel development

--- kernel.c
+++ kernel.c
@@ -19,3 +19,14 @@ void kernel_main(void){
     memset(__bss,0,(size_t)__bss_end-(size_t)__bss);
     for(;;);
 }
+
+__attribute__((section(".text.book")))
+__attribute__((naked))
+void boot(void){
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n" 
+        "j kernel_main\n"       
+        :
+        : [stack_top] "r" (__stack_top) 
+    );
+}

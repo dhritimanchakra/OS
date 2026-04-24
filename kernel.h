a9745b0413bdab1fc9d44196d0902c009700b8f6
Dhrimanti <dhritimancg@gmail.com>
2026-04-24 20:03:02 +0530

    PLease help me kill myself

--- kernel.h
+++ kernel.h
@@ -5,3 +5,11 @@ struct sbiret{
     long value;
 
 };
+
+
+
+#define PANIC(fmt, ...)
+    do{
+         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        while (1) {}  
+    }while(0)

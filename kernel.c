64a2ce2b5509785a05596bef5093a18f53b9050b
Dhrimanti <dhritimancg@gmail.com>
2026-04-24 20:01:26 +0530

    PLease help me kill myself

--- kernel.c
+++ kernel.c
@@ -49,10 +49,9 @@ void *memset(void *buf,char c,size_t n){
 
 void kernel_main(void){
 
-    const char *s="\n\nHello World\n";
-    for(int i=0;s[i]!='\0';i++){
-        putchar(s[i]);
-    }
+    printf("\n\nHello %s\n", "World!");
+    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+    
 
     memset(__bss,0,(size_t)__bss_end-(size_t)__bss);
     for(;;){

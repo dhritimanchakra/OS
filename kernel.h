3c2ba1b5083b680c7b1349c6edc6b50177c7c34c
Dhrimanti <dhritimancg@gmail.com>
2026-04-29 16:05:45 +0530

    I want to die

--- kernel.h
+++ kernel.h
@@ -1,12 +1,13 @@
 #pragma once
-
-#define STAP_SV32(1u<<32)
-#define PAGE_V (1<<0)
-#define PAGE_R (1<<1)
-#define PAGE_W (1<<2)
-#define PAGE_X (1<<3)
-#define PAGE_U (1<<4)
 #include "common.h"
+
+#define SATP_SV32   (1u << 31)
+#define PAGE_SIZE   4096
+#define PAGE_V      (1 << 0)
+#define PAGE_R      (1 << 1)
+#define PAGE_W      (1 << 2)
+#define PAGE_X      (1 << 3)
+#define PAGE_U      (1 << 4)
 
 struct sbiret{
     long error;
@@ -72,6 +73,7 @@ struct process{
     int pid;
     int state;
     vaddr_t sp;
+    uint32_t *page_table;
     uint8_t stack[8192];
 };
 

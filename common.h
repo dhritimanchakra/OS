2d85041df829ad801ef34f33ace75b1509be8e8c
Dhrimanti <dhritimancg@gmail.com>
2026-05-12 19:32:18 +0530

    Debuggin still going on

--- common.h
+++ common.h
@@ -15,8 +15,8 @@ typedef uint32_t vaddr_t;
 #define true 1
 #define false 0
 #define NULL ((void *)0)
-#define align_up(value,align) __builtin_align_up(value,align)
-#define is_aligned(value,align) __builtin_align_up(value,align)
+#define align_up(value, align)   __builtin_align_up(value, align)
+#define is_aligned(value, align) (((value) & ((align) - 1)) == 0)
 #define offset_of(type,member) __builtin_offsetof(type, member)
 #define va_list __builtin_va_list
 #define va_start __builtin_va_start

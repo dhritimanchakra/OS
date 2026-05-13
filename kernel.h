85d46f694b6deeb3a05af7d5ad5faf7f1852f440
Dhrimanti <dhritimancg@gmail.com>
2026-05-13 12:10:34 +0530

    Still working on this

--- kernel.h
+++ kernel.h
@@ -7,6 +7,30 @@
 #define SYS_PUTCHAR 1
 #define SYS_GETCHAR 2
 #define PROC_EXITED 2
+#define SECTOR_SIZE       512
+#define VIRTQ_ENTRY_NUM   16
+#define VIRTIO_DEVICE_BLK 2
+#define VIRTIO_BLK_PADDR  0x10001000
+#define VIRTIO_REG_MAGIC         0x00
+#define VIRTIO_REG_VERSION       0x04
+#define VIRTIO_REG_DEVICE_ID     0x08
+#define VIRTIO_REG_PAGE_SIZE     0x28
+#define VIRTIO_REG_QUEUE_SEL     0x30
+#define VIRTIO_REG_QUEUE_NUM_MAX 0x34
+#define VIRTIO_REG_QUEUE_NUM     0x38
+#define VIRTIO_REG_QUEUE_PFN     0x40
+#define VIRTIO_REG_QUEUE_READY   0x44
+#define VIRTIO_REG_QUEUE_NOTIFY  0x50
+#define VIRTIO_REG_DEVICE_STATUS 0x70
+#define VIRTIO_REG_DEVICE_CONFIG 0x100
+#define VIRTIO_STATUS_ACK       1
+#define VIRTIO_STATUS_DRIVER    2
+#define VIRTIO_STATUS_DRIVER_OK 4
+#define VIRTQ_DESC_F_NEXT          1
+#define VIRTQ_DESC_F_WRITE         2
+#define VIRTQ_AVAIL_F_NO_INTERRUPT 1
+#define VIRTIO_BLK_T_IN  0
+#define VIRTIO_BLK_T_OUT 1
 
 #define USER_BASE 0x1000000
 #define SATP_SV32   (1u << 31)
@@ -79,6 +103,7 @@ struct trap_frame {
 #define PROC_RUNNABLE 1
 
 
+struct 
 struct process{
     int pid;
     int state;
--- run.sh
+++ run.sh
@@ -5,6 +5,12 @@ QEMU=/home/linuxbrew/.linuxbrew/Cellar/qemu/10.2.2/bin/qemu-system-riscv32
 CC=clang
 OBJCOPY=llvm-objcopy  # or: $(llvm-config --bindir)/llvm-objcopy
 
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=lorem.txt,format=raw,if=none \            
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \  
+    -kernel kernel.elf
+
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf \
   -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
@@ -17,5 +23,3 @@ $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
   kernel.c common.c shell.bin.o
 
 
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-  -kernel kernel.elf

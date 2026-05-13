6cb1decaf11dcdbdf6729cc6b6807543eea79c33
Dhrimanti <dhritimancg@gmail.com>
2026-05-13 12:34:43 +0530

    The kernel.h is being filled and disk is being written

--- kernel.h
+++ kernel.h
@@ -103,7 +103,53 @@ struct trap_frame {
 #define PROC_RUNNABLE 1
 
 
-struct 
+struct virtq_desc{
+    uint64_t addr;
+    uint32_t len;
+    uint16_t flags;
+    uint16_t next;
+
+}__attribute__((packed));
+
+struct virtq_avail{
+    uint16_t flags;
+    uint16_t index;
+    uint16_t ring[VIRTQ_ENTRY_NUM];
+
+}__attribute__((packed));
+
+struct virtq_used_elem{
+    uint32_t id;
+    uint32_t len;
+}__attribute__((packed));
+
+struct virtq_used{
+    uint16_t flags;
+    uint16_t index;
+    struct virtq_used_elem ring[VIRTQ_ENTRY_NUM];
+
+}__attribute__((packed));
+
+
+struct virtio_virtq{
+    struct virtq_desc descs[VIRTQ_ENTRY_NUM];
+    struct virtq_avail avail;
+    struct virtq_used used __attribute__((aligned(PAGE_SIZE)));
+    int queue_index;
+    volatile uint16_t *used_index;
+    uint16_t last_used_index;
+
+}__attribute__((packed));
+
+struct virtio_blk_req{
+    uint32_t type;
+    uint32_t reserved;
+    uint64_t sector;
+    uint8_t data[512];
+    uint8_t status;
+
+}__attribute__((packed));
+
 struct process{
     int pid;
     int state;
